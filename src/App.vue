9979f61344417b316e6b2c1fe55eec5c885e4fdf
Khoding <julien.rutscho@ceff.ch>
2025-06-05 01:02:44 +0200

    So yeah, if you don't use RouterLink, Vue Router doesn't really work lmao

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <div><a href="/link-images">Link Images</a></div>
-  <div><a href="/websites">Websites</a></div>
-  <p><a href="/">Back</a></p>
+  <div><RouterLink to="/link-images">Link Images</RouterLink></div>
+  <div><RouterLink to="/websites">Websites</RouterLink></div>
+  <p><RouterLink to="/">Back</RouterLink></p>
 
   <RouterView />
 </template>
