b79e733d47a646ac5f1577a01655458c8e5142a2
Khoding <julien.rutscho@ceff.ch>
2025-06-05 19:28:08 +0200

    uhm is that it?

--- src/main.js
+++ src/main.js
@@ -6,6 +6,14 @@ import router from '@/router'
 
 const app = createApp(App)
 
+router.isReady().then(() => {
+  const redirectUrl = localStorage.getItem('redirectUrl')
+  if (redirectUrl) {
+    router.push(redirectUrl)
+    localStorage.removeItem('redirectUrl')
+  }
+})
+
 app.use(router)
 
 app.mount('#app')
